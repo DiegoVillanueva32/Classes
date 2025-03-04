baa2c4a53461bdcd0120ef415d3c40c38ad2c644
Sala315 <d.villanueva0504@gmail.com>
2025-03-03 19:18:49 -0500

    Student Creation

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Student diego = new Student("Diego", "000565383", 16, 'm');
-        Student cristian = new Student();
-        System.out.println(diego);
-        System.out.println(cristian);
-        System.out.println(diego.studentId);
-        System.out.println(cristian.studentId);
+        Scanner scanner = new Scanner(System.in);
 
+        // Crear instancias de Student
+        Student diego = new Student("Diego", "000565383", 16, 'm', "d.villanueva0504@gmail.com");
+        Student cristian = new Student();
+
+
+        System.out.println("Información de Diego:");
+        diego.tellInfo();
+        System.out.println("ID de Diego: " + diego.studentId);
+
+        diego.repeatAfter("Pequeño demoniooo!!");
+
+
+        System.out.print("¿Has cumplido años? (si/no): ");
+        String respuesta = scanner.nextLine();
+
+        if (respuesta.equalsIgnoreCase("si")) {
+            diego.haveBirthday();
+            System.out.println("¡Feliz cumpleaños, " + diego.studentName + "! Ahora tienes " + diego.studentAge + " años.");
+        } else {
+            System.out.println("No se realizaron cambios en la edad.");
+        }
+
+        scanner.close();
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,21 +1,37 @@
 
 
 public class Student {
-    //Instance Fields
+    // Instance Fields
     String studentName;
     String studentId = "000000000";
     int studentAge;
     char studentGender;
+    String studentEmail;
 
-    //Constructor
+    // Constructor
     public Student() {
         System.out.println("Estudiante creado sin datos!");
     }
-    public Student(String name, String id, int age, char gender) {
-        studentName = name;
-        studentId = id;
-        studentAge = age;
-        studentGender = gender;
-        System.out.println("Estudiante \"" + studentName + "\" creado!");
+
+    public Student(String name, String id, int age, char gender, String email) {
+        this.studentName = name;
+        this.studentId = id;
+        this.studentAge = age;
+        this.studentGender = gender;
+        this.studentEmail = email;
+        System.out.println("Estudiante \"" + this.studentName + "\" creado!");
+    }
+
+    public void tellInfo() {
+        System.out.println("My name is \"" + studentName + "\" and my ID is " + studentId + ".");
+        System.out.println("I'm " + studentAge + " years old and my gender is " + studentGender + ".");
+    }
+
+    public void repeatAfter(String message) {
+        System.out.println("The message is: " + message);
+    }
+
+    public void haveBirthday() {
+        studentAge++;
     }
 }
